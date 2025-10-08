294ea2868cf432c06ff3a4e9f9b899983898169c
lujay7 <93889784+lujay7@users.noreply.github.com>
2025-10-08 18:30:33 -0400

    Update Template.java

--- Template.java
+++ Template.java
@@ -1,1 +1,26 @@
-ECHO is on.
+/**
+* This class is a template of what a normal Java class should look like.
+* It includes documentation, a constructor, and a main method.
+* Running this 
+*
+* @author David Lu
+* @since Oct 8 2025
+*/
+public class Template
+{
+  /**
+  * Most classes should include a default constructor
+  */
+  public Template(){}
+  
+  /**
+  * The main method where every Java program starts when ran.
+  * 
+  * @param args command-line arguments passed to the method.
+  */
+  public static void main(String[] args)
+  {
+    //prints Hello World to the console
+    System.out.println("Hello World!");
+  }
+}
