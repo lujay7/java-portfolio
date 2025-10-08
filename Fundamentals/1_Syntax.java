93d6ebe58d72f42a8c288f3ca03891eb2a39ba7c
lujay7 <93889784+lujay7@users.noreply.github.com>
2025-10-08 16:16:33 -0400

    Update and rename 1.Syntax.java to 1_Syntax.java

---
+++ Fundamentals/1_Syntax.java
@@ -0,0 +1,7 @@
+/**
+* This class demonstrates a basic hello world program.
+* It shows how to create a class, define the main method, and output text with a print statment.
+*
+* @author David Lu
+*/
+public class HelloWorld{} 
