1d312fdb233f5f8d03ab6d2a69ff1dc096d01f8e
lujay7 <93889784+lujay7@users.noreply.github.com>
2025-10-15 18:07:27 -0400

    Update _8_Packages.java

--- 1-fundamentals/_8_Packages.java
+++ 1-fundamentals/_8_Packages.java
@@ -1,1 +1,31 @@
-ECHO is on.
+//Imports the 3 most useful java packages.
+import java.lang.*
+import java.util.*
+import java.io.*;
+
+/**
+* This class imports packages and explains there uses.
+* It will demonstrate several uses of common packages used in java.
+*
+* @author David Lu
+* @since Oct 15 2025
+*/
+public class _8_Packages
+{
+  /**
+  * Default Constructor.
+  */
+  public _8_Packages(){}
+  
+  /**
+  * Main Method.
+  * Prints out a statment.
+  * 
+  * @param args command-line arguments passed to the method.
+  */
+  public static void main(String[] args)
+  {
+    //prints Template Works to the console.
+    System.out.println("Template Works");
+  }
+}
