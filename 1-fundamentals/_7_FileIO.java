2519d09d81948f739edf4ee8659ed2bf4e38b10a
lujay7 <93889784+lujay7@users.noreply.github.com>
2025-10-15 17:45:23 -0400

    Update _7_FileIO.java

--- 1-fundamentals/_7_FileIO.java
+++ 1-fundamentals/_7_FileIO.java
@@ -1,27 +1,49 @@
+//Imports classes and packages from java.
+import java.util.*;
+import java.io.*;
+
 /**
-* This class is a template of what a normal Java class should look like.
-* It includes documentation, a constructor, and a main method.
-* Running this program will output a simple statment "Template Works".
+* This class demonstrates how to input and output data in java with user input and files.
+* It contains use of the scanner class, the bufferedreader class, and the filereader class.
+* This class also shows how to use try catch to prevent errors from occuring
 *
 * @author David Lu
 * @since Oct 15 2025
 */
-public class Template
+public class _7_FileIO
 {
   /**
   * Default Constructor.
   */
-  public Template(){}
+  public _7_FileIO(){}
   
   /**
   * Main Method.
-  * Prints out a statment.
+  * Creates a scanner object and inputs a value.
+  * Creates a bufferedreader object and reads text from a file.
   * 
   * @param args command-line arguments passed to the method.
   */
   public static void main(String[] args)
   {
-    //prints Template Works to the console.
-    System.out.println("Template Works");
+    //Creates a scanner object to take an input
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("What is your name?");
+    String name = scanner.nextLine();
+    System.out.println();
+    System.out.println("Your name is: " + name);
+
+    System.out.println();
+    try(BufferedReader reader = new BufferedReader(new FileReader("readFile.txt")))
+    {
+      String line;
+      while(line = reader.nextLine() != null)
+      {
+        System.out.println(line);
+      }
+    }
+    catch(IO Exception e)
+    {
+      System.out.println("Was not able to read File: " + e.getMessage());
   }
 }
