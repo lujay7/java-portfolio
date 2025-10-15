40bea10765b7875ccc82b82595ea604123c8c022
lujay7 <93889784+lujay7@users.noreply.github.com>
2025-10-15 15:41:09 -0400

    Update _7_Classes.java

--- 1-fundamentals/_7_Classes.java
+++ 1-fundamentals/_7_Classes.java
@@ -1,1 +1,27 @@
-ECHO is on.
+/**
+* This class is a template of what a normal Java class should look like.
+* It includes documentation, a constructor, and a main method.
+* Running this program will output a simple statment "Template Works".
+*
+* @author David Lu
+* @since Oct 8 2025
+*/
+public class Template
+{
+  /**
+  * Default Constructor.
+  */
+  public Template(){}
+  
+  /**
+  * Main Method.
+  * Prints out a statment.
+  * 
+  * @param args command-line arguments passed to the method.
+  */
+  public static void main(String[] args)
+  {
+    //prints Template Works to the console.
+    System.out.println("Template Works");
+  }
+}
